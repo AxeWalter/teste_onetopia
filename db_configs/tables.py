a03650e2f176e83da9e34d030134ec90a4b7637d
Valter Junior <valter.machado.junior@outlook.com>
2026-03-03 23:14:24 -0300

    versão final do CREATE table

--- db_configs/tables.py
+++ db_configs/tables.py
@@ -15,18 +15,18 @@ class Cryptos(Base):
 class Monitoring(Base):
     __tablename__ = 'monitoring'
 
-    # Os números de precisão parecem aleatórios, mas há um motivo. Usar 25 como máximo de precisão vem do fato que
-    # em vários testes o manior número de casas decimais foi 20, logo, uso 25 para ter uma segurança. Price usa 20
-    # de escala pois há algumas "memecoins" que chegam a 18 casas decimais após a vírgula (por isso o aumento no total
-    # de dígitos também. Os demais que usam 2 é devido ao fato que as casas decimais não são tão relevantes.
+    # Os números de precisão parecem aleatórios, mas há um motivo. Usar 20 como máximo de precisão vem do fato que
+    # em vários testes o manior número de casas decimais foi 20. Price usa 18 de escala pois há algumas "memecoins" que
+    # chegam a 18 casas decimais após a vírgula (por isso o aumento no total de dígitos também). Os demais que usam 2 é
+    # devido ao fato que as casas decimais não são tão relevantes.
 
     id = Column(Integer, primary_key=True)
     crypto_id = Column(Integer, ForeignKey('cryptos.id'))
-    price = Column(Numeric(40,20), nullable=False)
+    price = Column(Numeric(36,18), nullable=False)
     rank = Column(Integer)
-    circulating_supply = Column(Numeric(25,2))
-    market_cap = Column(Numeric(25,2))
-    volume_24h = Column(Numeric(25,2))
+    circulating_supply = Column(Numeric(20,2))
+    market_cap = Column(Numeric(20,2))
+    volume_24h = Column(Numeric(20,2))
     percent_change_24h = Column(Numeric(5,2))
     timestamp = Column(DateTime, nullable=False)
 
