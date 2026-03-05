3b420bf10f83c94d9e6099ff271b0754cc5cfade
Valter Junior <valter.machado.junior@outlook.com>
2026-03-05 10:59:48 -0300

    feat: logging foi adicionado

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from conexaoAPI import request_api
 from extracao import parsing_data
 from db_configs.data_manipulation import insert_all
+import logging
 
 
-
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode="a",
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 def main():
-    number_of_cryptos = 200  # 200 cryptos = 1 token da API
-    currency = "BRL"
-    api_data = request_api(number_of_cryptos, "BRL")
+    try:
+        number_of_cryptos = 200  # 200 cryptos = 1 token da API
+        currency = "BRL"
 
-    clean_data = parsing_data(api_data)
+        logging.info("Iniciando coleta de dados da API...")
+        api_data = request_api(number_of_cryptos, currency)
+        logging.info(f"{len(api_data)} coletados com sucesso!")
 
-    insert_all(clean_data)
+        logging.info("Iniciando limpeza dos dados...")
+        clean_data = parsing_data(api_data)
+        logging.info("Dados limpos com sucesso!")
 
+        logging.info("Iniciando insercao de dados no Banco...")
+        insert_all(clean_data)
+        logging.info("Dados inseridos com sucesso!")
 
+    except Exception as e:
+        logging.error(e, exc_info=True)
 
+    finally:
+        logging.info("-" * 50)
 
 
 if __name__ == '__main__':
